34993a47ddd4b80da05e3bc59ed7253202b34e4f
kRHYME7 <53417443+kRHYME7@users.noreply.github.com>
2025-06-11 10:19:42 +0800

    remove the  annoying translation banner and use tooltip

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,9 @@ export default defineConfig({
         // Path to our custom CSS file
         './src/styles/custom.css',
       ],
+      components: {
+        LanguageSelect: './src/components/LanguageSelect.astro',
+      },
       defaultLocale: 'en',
       locales: {
         en: { label: 'English', lang: 'en' },
